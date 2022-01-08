8840587d9a6914561985d124dd85d7bda11947cc
dtlnor <dtlnor@hotmail.com>
2022-01-09 01:52:21 +0900

    Update Lottery.py

--- Lottery.py
+++ Lottery.py
@@ -16,7 +16,16 @@ def GetOnePage(url):
 
     #Return code, return 0 if reposters exist
     rescode = resJson['code']
-    
+    if (rescode > 0):
+        has_more = 0
+        offset = ""
+    else:
+        has_more = resJson['data']['has_more']
+        if (has_more > 0):
+            offset = str(resJson['data']['offset'])
+        else:
+            offset=""
+     
     if rescode == 0:
         #获取所单页的转发元素
         items = resJson['data']['items']
@@ -24,27 +33,29 @@ def GetOnePage(url):
             itemJson = json.loads(item['card'])
             #定位到UID
             resList.append(itemJson['user']['uid'])
-    
-    return [rescode, resList]
+
+    return [has_more, rescode, offset, resList]
 
 def GetPagesCycle(url):
     allUIDSet = []
 
     #获取第一页
     firstPageGet = GetOnePage(url)
-    retCode = firstPageGet[0]
-    allUIDSet += firstPageGet[1]
-
+    hasMore = firstPageGet[0]
+    retCode = firstPageGet[1]
+    offset = firstPageGet[2]
+    allUIDSet += firstPageGet[3]
     offsetIndex = 20
-    while retCode == 0:
+    while retCode == 0 and hasMore == 1:
         #偏移量增加，生成下一页的url
-        offsetUrl = url + '&offset=1:' + str(offsetIndex)
+        offsetUrl = url + '&offset=' + str(offset)
         currentPage = GetOnePage(offsetUrl)
-        retCode = currentPage[0]
-        allUIDSet += currentPage[1]
-
-        offsetIndex += 20
-    
+        print(currentPage)
+        hasMore = currentPage[0]
+        retCode = currentPage[1]
+        offset = currentPage[2]
+        allUIDSet += currentPage[3]
+        
     return allUIDSet
 
 #随机方法，传入UID列表
@@ -56,8 +67,11 @@ def GetOneLuckyDog(uidList):
 print('Bilibili转发抽奖工具v1.0')
 print('Bilibili@鱼丸子_Official')
 
-url = 'https://api.vc.bilibili.com/dynamic_repost/v1/dynamic_repost/repost_detail?dynamic_id=' + input('输入动态号')
+url = 'https://api.vc.bilibili.com/dynamic_repost/v1/dynamic_repost/repost_detail?dynamic_id=' + '539142451483532317'
+# 539142451483532317
 repostList = list(dict.fromkeys(GetPagesCycle(url)))
+print("what")
+print(repostList)
 print('成功获取转发列表')
 generateNum = int(input('输入随机数量'))
 
